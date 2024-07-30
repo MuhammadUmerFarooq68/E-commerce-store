a98b0997bae291ca7ae998592205496198c10a85
umerfarooq68 <engr.umerfarooq68@gmail.com>
2024-07-30 17:23:54 +0500

    testing commit

--- server.js
+++ server.js
@@ -17,5 +17,6 @@ sequelize.sync({ force: false }).then(() => {
   console.log('Database synced');
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('server is on')
   });
 });
